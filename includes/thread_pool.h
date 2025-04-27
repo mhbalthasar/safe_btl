40cfb4a19695113adc49a732b9d612f2770a94a7
mhbalthasar <mhbalthasar@cmcc.cc>
2025-04-27 20:18:06 +0800

    Update thread_pool.h

--- includes/thread_pool.h
+++ includes/thread_pool.h
@@ -14,74 +14,76 @@
 #include <functional>
 #include <stdexcept>
 
-class thread_pool {
-public:
-    explicit thread_pool(size_t threads = std::thread::hardware_concurrency())
-            : stop(false) {
-        if(threads == 0) threads = 1;  // 至少1个线程
-        for(size_t i = 0; i < threads; ++i) {
-            workers.emplace_back([this] {
-                for(;;) {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                                             [this]{ return this->stop || !this->tasks.empty(); });
+namespace btl {
+    class thread_pool {
+    public:
+        explicit thread_pool(size_t threads = std::thread::hardware_concurrency())
+                : stop(false) {
+            if (threads == 0) threads = 1;  // 至少1个线程
+            for (size_t i = 0; i < threads; ++i) {
+                workers.emplace_back([this] {
+                    for (;;) {
+                        std::function<void()> task;
+                        {
+                            std::unique_lock<std::mutex> lock(this->queue_mutex);
+                            this->condition.wait(lock,
+                                                 [this] { return this->stop || !this->tasks.empty(); });
 
-                        if(this->stop && this->tasks.empty())
-                            return;
+                            if (this->stop && this->tasks.empty())
+                                return;
 
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
+                            task = std::move(this->tasks.front());
+                            this->tasks.pop();
+                        }
+                        try {
+                            task();  // 执行任务
+                        } catch (...) {
+                            // TODO 异常处理
+                        }
                     }
-                    try {
-                        task();  // 执行任务
-                    } catch(...) {
-                        // TODO 异常处理
-                    }
-                }
-            });
+                });
+            }
         }
-    }
 
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args)
-    -> std::future<typename std::result_of<F(Args...)>::type> {
-        using return_type = typename std::result_of<F(Args...)>::type;
+        template<class F, class... Args>
+        auto enqueue(F &&f, Args &&... args)
+        -> std::future<typename std::result_of<F(Args...)>::type> {
+            using return_type = typename std::result_of<F(Args...)>::type;
 
-        auto task = std::make_shared<std::packaged_task<return_type()>>(
-                std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
+            auto task = std::make_shared<std::packaged_task<return_type()>>(
+                    std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            );
 
-        std::future<return_type> res = task->get_future();
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            if(stop)
-                throw std::runtime_error("enqueue on stopped ThreadPool");
+            std::future<return_type> res = task->get_future();
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                if (stop)
+                    throw std::runtime_error("enqueue on stopped ThreadPool");
 
-            tasks.emplace([task](){ (*task)(); });
+                tasks.emplace([task]() { (*task)(); });
+            }
+            condition.notify_one();
+            return res;
         }
-        condition.notify_one();
-        return res;
-    }
 
-    ~thread_pool() {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
+        ~thread_pool() {
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+                stop = true;
+            }
+            condition.notify_all();
+            for (std::thread &worker: workers)
+                if (worker.joinable())
+                    worker.join();
         }
-        condition.notify_all();
-        for(std::thread &worker : workers)
-            if(worker.joinable())
-                worker.join();
-    }
 
-private:
-    std::vector<std::thread> workers;
-    std::queue<std::function<void()>> tasks;
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
-};
+    private:
+        std::vector<std::thread> workers;
+        std::queue<std::function<void()>> tasks;
+        std::mutex queue_mutex;
+        std::condition_variable condition;
+        bool stop;
+    };
+}
 
 #endif //XIPC_THREAD_POOL_H
